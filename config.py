d26c1751c24463b371309c2f4d690b3abb557f52
leonalib <ayrkinbek@gmail.com>
2026-06-16 16:06:57 +0500

    fix: use env variables in config

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-TOKEN = os.environ.get("BOT_TOKEN")
-ADMIN_IDS = [8419513332]
+import os
 
-DB_NAME = "Cake_Helper"
-DB_USER = "postgres"
-DB_PASSWORD = "1234"
-DB_HOST = "localhost"
+TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_IDS = [int(os.environ.get("ADMIN_ID", 8419513332))]
+
+DB_NAME = os.environ.get("DB_NAME", "Cake_Helper")
+DB_USER = os.environ.get("DB_USER", "postgres")
+DB_PASSWORD = os.environ.get("PGPASSWORD")
+DB_HOST = os.environ.get("PGHOST")
